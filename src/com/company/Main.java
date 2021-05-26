b3538e40409d3fbcfa973d8853f0265a66805d69
NuraiymIbraeva <nurayymi@list.ru>
2021-05-26 16:50:20 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Узнали количество элементов в массиве
+
+        //Тип данных double
+        double[] myArray1 = {2.4, 3.8, 11.2, 9.8, 1.18};
+        System.out.println("Количество элементов в массиве myArray1: " + myArray1.length);
+        //Тип String
+        String[] myArray2 = {"Java", "массив", "пример", "JavaScript"};
+        System.out.println("Количество элементов в массиве myArray2: " + myArray2.length);
+
+        // Массив myArrayInt6 из 10 элементов с начальным значением 0
+        int [] myArrayInt6 = new int [10];
+        myArray1[2] = 8;
+
+
+    }}
+
+
